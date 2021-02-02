a5890ebe42ea204111fb73c4b75947056b441f43
VK <indira_win@hotmail.com>
2021-02-02 22:09:32 +0000

    Working Model

---
+++ src/main/java/pageEvents/LoginPageEvents.java
@@ -0,0 +1,18 @@
+package main.java.pageEvents;
+
+import main.java.pageObjects.LoginPageObjects;
+import main.java.utils.ElementFetch;
+import org.testng.Assert;
+
+public class LoginPageEvents {
+    public void amazonLoginScreenValidation() throws Exception{
+        ElementFetch elementFetch = new ElementFetch();
+        Assert.assertTrue(elementFetch.getListWebElements("XPATH", LoginPageObjects.loginText).size()>0,"Amazon");
+    }
+
+    public void enterEmailAddress(String emailId) throws Exception{
+        ElementFetch elementFetch = new ElementFetch();
+        elementFetch.getWebElement("ID",LoginPageObjects.emailAddress).sendKeys(emailId);
+        ///////elementFetch.getWebElement("ID",LoginPageObjects.emailAddress).
+    }
+}
---
+++ src/test/java/AmazonLoginPageTest.java
@@ -0,0 +1,21 @@
+package test.java;
+
+import main.java.pageEvents.HomePageEvents;
+import main.java.pageEvents.LoginPageEvents;
+import org.testng.annotations.Test;
+
+
+public class AmazonLoginPageTest extends BaseTest{
+    @Test
+    public void enteringEmailAddress() throws Exception{
+        HomePageEvents homePageEvents = new HomePageEvents();
+        homePageEvents.amazonHomeScreenValidation();
+        LoginPageEvents loginPageEvents = new LoginPageEvents();
+        loginPageEvents.amazonLoginScreenValidation();
+        loginPageEvents.enterEmailAddress("vkr@vkr.com");
+
+        System.out.println("Hi - this is @Test");
+    }
+
+
+}
